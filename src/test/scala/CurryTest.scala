3e0ebd5fde4746560065d3307c05b2bb93976e7d
pfried <philipsfried@gmail.com>
2015-08-04 17:47:58 -0400

    added test for composing functions

--- src/test/scala/CurryTest.scala
+++ src/test/scala/CurryTest.scala
@@ -22,3 +22,31 @@ class CurryTest extends FunSpec {
 
   }
 }
+
+
+
+class ComposeTest extends FunSpec {
+
+  def compose[A, B, C](f1: A => B, f2: B => C): A => C = {
+    (a: A) => f2(f1(a))
+  }
+
+  describe("composing functions") {
+
+    def fun1(i: Int): String = {
+      i.toString()
+    }
+
+    def fun2(s: String): Seq[String] = {
+      Seq.fill(3)(s)
+    }
+
+    it("composes two functions") {
+      val composed: Int => Seq[String] = compose(fun1, fun2)
+
+      val result: Seq[String] = composed(5)
+
+      assert(Seq("5", "5", "5") == result)
+    }
+  }
+}
